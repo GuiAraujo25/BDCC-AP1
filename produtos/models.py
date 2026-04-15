378ab6d0a56c8f2c9dc08aa94b6fc86e1b41c2bf
GuiAraujo25 <guilhermepardelhas@gmail.com>
2026-04-15 15:10:59 -0300

    Implementação de Categorias e configurações de deploy AWS

--- produtos/models.py
+++ produtos/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
 class Produto(models.Model):
     nome = models.CharField(max_length=200)
-    descricao = models.TextField()
+    descricaao = models.TextField() 
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     imagem = models.ImageField(upload_to='produtos/', blank=True, null=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
+    
+    # Relacionamento com Categoria
+    categoria = models.ForeignKey(
+        Categoria, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        related_name='produtos'
+    )
 
     def __str__(self):
         return self.nome
--- produtos/serializers.py
+++ produtos/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Produto
+from .models import Produto, Categoria
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categoria
+        fields = '__all__'
 
 class ProdutoSerializer(serializers.ModelSerializer):
+    # Isso faz aparecer o nome da categoria no JSON da API
+    categoria_detalhes = CategoriaSerializer(source='categoria', read_only=True)
+
     class Meta:
         model = Produto
-        fields = ['id', 'nome', 'descricao', 'preco', 'imagem', 'data_criacao']
+        fields = ['id', 'nome', 'descricaao', 'preco', 'imagem', 'data_criacao', 'categoria', 'categoria_detalhes']
